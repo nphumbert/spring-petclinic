b096bf92c48d12cf5b3df9651194804fe74e07c7
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2016-07-07 12:03:28 +0200

    refactor(config): Replace XML config by Java config for tools

--- src/main/java/org/springframework/samples/petclinic/ToolsConfig.java
+++ src/main/java/org/springframework/samples/petclinic/ToolsConfig.java
@@ -1,9 +1,33 @@
 package org.springframework.samples.petclinic;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.samples.petclinic.util.CallMonitoringAspect;
 
 @Configuration
-@ImportResource("classpath:spring/tools-config.xml")
+@EnableAspectJAutoProxy
+@EnableMBeanExport
+@EnableCaching
 public class ToolsConfig {
+
+    @Bean
+    public CallMonitoringAspect callMonitor() {
+        return new CallMonitoringAspect();
+    }
+
+    @Bean
+    public CacheManager cacheManager(net.sf.ehcache.CacheManager cacheManager) {
+        return new EhCacheCacheManager(cacheManager);
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehcache() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("cache/ehcache.xml"));
+        return ehCacheManagerFactoryBean;
+    }
 }
